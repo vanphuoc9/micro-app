c0114c100e1e06c8b41e55bd6f8b7bc20f1b4adb
phuoctv.tgg <thaiphuoc1997@gmail.com>
2021-01-29 21:31:25 +0700

    init host shell

---
+++ acme-email-client/src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from "@angular/router";
+
+const routes: Routes = [
+  {
+    path: 'mailbox',
+    loadChildren: () => import('mailbox/MailboxModule').then(m => m.MailboxModule)
+  },
+  {
+    path: 'calendar',
+    loadChildren: () => import('calendar/CalendarModule').then(m => m.CalendarModule)
+  }
+];
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, {
+    enableTracing: true,
+  })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
